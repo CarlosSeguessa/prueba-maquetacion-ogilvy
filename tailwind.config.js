3a919ce52a670d32e2e1fb07813b649d8af7a3bd
Carlos Seguessa <carlosseguessa@gmail.com>
2023-03-29 19:22:09 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            'primary-green':'#37B4A7',
+            'secondary-orange-400':'#FF8D4E',
+            'secondary-orange-100':'#FFE0CA',
+        },
+        fontFamily: {
+            'poppins': ['Poppins', 'sans-serif'],
+            'rubik': ['Rubik', 'sans-serif'],
+        }
+      },
+    },
+    plugins: [],
+  }
